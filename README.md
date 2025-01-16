df4841b45cb49353cfaa1f381921972fb764f0f7
Emerson <70976306+emerdesenv@users.noreply.github.com>
2025-01-16 11:14:06 -0300

    refactor: Ajustes doc

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 # teste Maestro
 
-Testes com Maestro
+* Testes na interface WEB + VS Code e Emulador
 
-* Seguir os documentos em anexo
+## Testes com Maestro
+
+* Seguir os documentos **script_init.txt** e **script_config_windows.sh**
+
+## Comandos básicos
+
+**Observações**: Se estiver rodando no Linux ou MAC não precisa colocar o **IPV4_WINDOWS**
+
+* No terminal do ubuntu, rodar: **maestro --host IPV4_WINDOWS test android-flow.yaml**
+
+## Documentações de Referências
+
+* **Mestro Framework** - https://maestro.mobile.dev/
--- script_config_windows.sh
+++ script_config_windows.sh
@@ -1,10 +1,28 @@
 # Instalação do Java
+# Instalação do Android Studio
 
 # https://github.com/hmsjy2017/get-jdk?tab=readme-ov-file
 
+# Configuração do Android Studio
+
+# (Emulador externo) - Seguir as configurações do tutorial: https://developer.android.com/studio/run/emulator-launch-separate-window?hl=pt-br
+# (Configuração pasta tools) - Conferir as evidências
+
+# Configurações das váriaveis de usuário
+
+# ANDROID_HOME = C:\Users\emers\AppData\Local\Android\Sdk
+# JAVA_HOME = C:\Program Files\Java\jdk1.8.0_231
+
+# Configurações das váriaveis de ambiente: PATH
+
+# C:\Program Files\Java\jdk1.8.0_231\bin
+# C:\Program Files\Java\jre1.8.0_231
+# C:\Users\emers\AppData\Local\Android\Sdk\tools
+# C:\Users\emers\AppData\Local\Android\Sdk\platform-tools
+
 #!/bin/bash
 
-# Configurações de atualizações do Ubuntu
+# Configurações no terminal do Ubuntu
 
 sudo apt update
 sudo apt upgrade
@@ -12,10 +30,6 @@ sudo apt upgrade
 # Configurações de instação do JDK
 
 sudo apt install openjdk-11-jdk
-
-# Pré configuração do Maestro
-
-curl -Ls "https://get.maestro.mobile.dev" | bash
 
 # Instalando unzip
 
@@ -49,7 +63,9 @@ mv cmdline-tools/* tools/
 
 # Configurações das variáveis de ambiente
 
+echo 'export MAESTRO_VERSION=1.37.0' >> ~/.bashrc
 echo 'export ANDROID_HOME=$HOME/Android' >> ~/.bashrc
+echo 'export PATH=$PATH:$ANDROID_HOME/platform-tools/bin/:$PATH' >> ~/.bashrc
 echo 'export PATH=$PATH:$ANDROID_HOME/tools/bin/:$PATH' >> ~/.bashrc
 echo 'export ANDROID_SDK_ROOT=$HOME/Android' >> ~/.bashrc
 
@@ -57,15 +73,10 @@ echo 'export ANDROID_SDK_ROOT=$HOME/Android' >> ~/.bashrc
 
 source ~/.bashrc
 
+# Pré configuração do Maestro
+
+curl -Ls "https://get.maestro.mobile.dev" | bash
+
 # Configurações SDK
 
 sdkmanager --install "platform-tools"
-
-# Última configuração da variáveis de ambiente
-
-echo 'export PATH=$PATH:$ANDROID_HOME/platform-tools/bin/:$PATH' >> ~/.bashrc
-
-# Configuração dAndroid Studio
-
-# (Emulador externo) - Seguir as configurações do tutorial: https://developer.android.com/studio/run/emulator-launch-separate-window?hl=pt-br
-# (Configuração pasta tools) - Conferir as evidências
